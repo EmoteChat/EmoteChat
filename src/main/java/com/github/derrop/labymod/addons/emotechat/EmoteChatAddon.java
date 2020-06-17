955e045ebd644ef5b0abbacc03305eced2f71cc3
derrop <derrop@outlook.de>
2020-06-17 15:10:47 +0200

    Initial commit

---
+++ src/main/java/com/github/derrop/labymod/addons/emotechat/EmoteChatAddon.java
@@ -0,0 +1,23 @@
+package com.github.derrop.labymod.addons.emotechat;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+import java.util.List;
+
+public class EmoteChatAddon extends LabyModAddon {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+}
